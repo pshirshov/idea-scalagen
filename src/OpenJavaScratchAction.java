6b9d5d94e02b204c7b95960b824df28ccecf61e4
pshirshov <pshirshov@gmail.com>
2016-07-04 14:44:40 +0100

    scratch buffer hotkeys

---
+++ src/OpenJavaScratchAction.java
@@ -0,0 +1,23 @@
+import com.intellij.lang.Language;
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.CommonDataKeys;
+import com.intellij.openapi.fileTypes.StdFileTypes;
+import com.intellij.openapi.project.Project;
+
+public class OpenJavaScratchAction extends AnAction {
+    private static final String JAVA_SCRATCH = "public class Scratch {\n" +
+            "    public static void main(String[] args) {\n" +
+            "        System.out.println(\"Hi!\");\n" +
+            "    }\n" +
+            '}';
+
+    @Override
+    public void actionPerformed(AnActionEvent e) {
+        final Project project = e.getRequiredData(CommonDataKeys.PROJECT);
+        Language language = StdFileTypes.JAVA.getLanguage();
+        ScalagenClass.doCreateNewScratch(project, false, language, JAVA_SCRATCH);
+    }
+}
+
+
--- src/ScalagenClass.java
+++ src/ScalagenClass.java
@@ -1,3 +1,8 @@
+import com.intellij.featureStatistics.FeatureUsageTracker;
+import com.intellij.ide.scratch.ScratchFileService;
+import com.intellij.ide.scratch.ScratchRootType;
+import com.intellij.lang.Language;
+import com.intellij.lang.StdLanguages;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.CommonDataKeys;
@@ -5,11 +10,16 @@ import com.intellij.openapi.command.WriteCommandAction;
 import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.editor.SelectionModel;
+import com.intellij.openapi.fileEditor.FileEditorManager;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.MessageType;
 import com.intellij.openapi.ui.popup.JBPopupFactory;
+import com.intellij.openapi.util.registry.Registry;
+import com.intellij.openapi.vfs.VirtualFile;
 import com.mysema.scalagen.ConversionSettings;
 import com.mysema.scalagen.Converter;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.plugins.scala.ScalaLanguage;
 
 
 public class ScalagenClass extends AnAction {
@@ -21,20 +31,34 @@ public class ScalagenClass extends AnAction {
         final Document document = editor.getDocument();
 
         final SelectionModel selectionModel = editor.getSelectionModel();
-        if (!selectionModel.hasSelection()) {
-            selectionModel.setSelection(0, document.getTextLength());
-        }
-        final int start = selectionModel.getSelectionStart();
-        final int end = selectionModel.getSelectionEnd();
 
         Runnable runnable = new Runnable() {
             @Override
             public void run() {
 
                 try {
+                    final String sourceToConvert;
+                    if (selectionModel.hasSelection()) {
+                        sourceToConvert = selectionModel.getSelectedText();
+                    } else {
+                        sourceToConvert = document.getText();
+                    }
+
                     ConversionSettings options = new ConversionSettings(true);
-                    String replacement = Converter.instance().convert(selectionModel.getSelectedText(), options);
-                    document.replaceString(start, end, replacement);
+                    String replacement = Converter.instance().convert(sourceToConvert, options);
+
+                    if (document.isWritable()) {
+                        if (selectionModel.hasSelection()) {
+                            final int start = selectionModel.getSelectionStart();
+                            final int end = selectionModel.getSelectionEnd();
+                            document.replaceString(start, end, replacement);
+                        } else {
+                            document.setText(replacement);
+                        }
+                    } else {
+                        Language language = getScalaLanguage();
+                        doCreateNewScratch(project, false, language, replacement);
+                    }
                 } catch (Throwable e) {
                     String message = "Failed to invoke scalagen: "+e.getMessage();
                     JBPopupFactory.getInstance()
@@ -47,5 +71,32 @@ public class ScalagenClass extends AnAction {
 
         WriteCommandAction.runWriteCommandAction(project, runnable);
         selectionModel.removeSelection();
+    }
+
+
+    public static Language getScalaLanguage() {
+        Language language = Language.findLanguageByID(ScalaLanguage.Instance.getID());
+        if (language == null) {
+            language = StdLanguages.TEXT;
+        }
+        return language;
+    }
+
+
+    static void doCreateNewScratch(@NotNull Project project, boolean buffer, @NotNull Language language, @NotNull String text) {
+        FeatureUsageTracker.getInstance().triggerFeatureUsed("scratch");
+
+        String fileName = buffer ? "buffer" + nextBufferIndex() : "scratch";
+        ScratchFileService.Option option = buffer ? ScratchFileService.Option.create_if_missing : ScratchFileService.Option.create_new_always;
+        VirtualFile f = ScratchRootType.getInstance().createScratchFile(project, fileName, language, text, option);
+        if (f != null) {
+            FileEditorManager.getInstance(project).openFile(f, true);
+        }
+    }
+
+    private static int ourCurrentBuffer = 0;
+    private static int nextBufferIndex() {
+        ourCurrentBuffer = (ourCurrentBuffer % Registry.intValue("ide.scratch.buffers")) + 1;
+        return ourCurrentBuffer;
     }
 }
